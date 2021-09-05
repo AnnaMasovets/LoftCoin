628f493ff2639b727c726aa18d69a94ed65de576
Shagunova Anna <masovets.aa@mail.ru>
2021-09-05 13:06:16 +0800

    work with homework 2

---
+++ app/src/main/java/com/aashagunova/loftcoin/data/Listings.java
@@ -0,0 +1,12 @@
+package com.aashagunova.loftcoin.data;
+
+import com.google.auto.value.AutoValue;
+
+import java.util.List;
+
+@AutoValue
+abstract class Listings {
+
+    abstract List<AutoValue_Coin> data();
+
+}
