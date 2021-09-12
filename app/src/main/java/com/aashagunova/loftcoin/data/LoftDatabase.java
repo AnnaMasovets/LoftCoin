687523e350612755467dd1a398c523758853d40b
Shagunova Anna <masovets.aa@mail.ru>
2021-09-12 20:36:07 +0800

    work with homework_3

--- app/src/main/java/com/aashagunova/loftcoin/data/LoftDatabase.java
+++ app/src/main/java/com/aashagunova/loftcoin/data/LoftDatabase.java
@@ -5,7 +5,8 @@ import androidx.room.RoomDatabase;
 
 @Database(entities = {
         RoomCoin.class
-}, version = 1)
+}, version = 1,
+        exportSchema = false)
 abstract class LoftDatabase extends RoomDatabase {
     abstract CoinsDao coins();
 }
